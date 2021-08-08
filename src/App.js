aa4c7f84e6dea9c15da79c05a5bae1eda90fba96
river248 <85404917+river248@users.noreply.github.com>
2021-08-09 00:03:54 +0700

    Design trello front-end.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,88 @@
-import './App.css'
+import './App.scss'
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="hello-world">Hello World</h1>
+    <div className="trello-riverdev-master">
+      <nav className="navbar app">App Bar</nav>
+      <nav className="navbar board">Board Bar</nav>
+      <div className="board-columns">
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://scontent.fdad3-3.fna.fbcdn.net/v/t1.6435-9/194192934_823035171966319_4289107895743838457_n.jpg?_nc_cat=108&ccb=1-4&_nc_sid=09cbfe&_nc_ohc=e1St53QL7MwAX96bxMK&_nc_ht=scontent.fdad3-3.fna&oh=b3db278d1be76affd28dbf94072dd77e&oe=6133B33F" alt="riverdev-alt-img"/>
+              Title: RIVERDEV
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://scontent.fdad3-3.fna.fbcdn.net/v/t1.6435-9/194192934_823035171966319_4289107895743838457_n.jpg?_nc_cat=108&ccb=1-4&_nc_sid=09cbfe&_nc_ohc=e1St53QL7MwAX96bxMK&_nc_ht=scontent.fdad3-3.fna&oh=b3db278d1be76affd28dbf94072dd77e&oe=6133B33F" alt="riverdev-alt-img"/>
+              Title: RIVERDEV
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://scontent.fdad3-3.fna.fbcdn.net/v/t1.6435-9/194192934_823035171966319_4289107895743838457_n.jpg?_nc_cat=108&ccb=1-4&_nc_sid=09cbfe&_nc_ohc=e1St53QL7MwAX96bxMK&_nc_ht=scontent.fdad3-3.fna&oh=b3db278d1be76affd28dbf94072dd77e&oe=6133B33F" alt="riverdev-alt-img"/>
+              Title: RIVERDEV
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://scontent.fdad3-3.fna.fbcdn.net/v/t1.6435-9/194192934_823035171966319_4289107895743838457_n.jpg?_nc_cat=108&ccb=1-4&_nc_sid=09cbfe&_nc_ohc=e1St53QL7MwAX96bxMK&_nc_ht=scontent.fdad3-3.fna&oh=b3db278d1be76affd28dbf94072dd77e&oe=6133B33F" alt="riverdev-alt-img"/>
+              Title: RIVERDEV
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://scontent.fdad3-3.fna.fbcdn.net/v/t1.6435-9/194192934_823035171966319_4289107895743838457_n.jpg?_nc_cat=108&ccb=1-4&_nc_sid=09cbfe&_nc_ohc=e1St53QL7MwAX96bxMK&_nc_ht=scontent.fdad3-3.fna&oh=b3db278d1be76affd28dbf94072dd77e&oe=6133B33F" alt="riverdev-alt-img"/>
+              Title: RIVERDEV
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App
